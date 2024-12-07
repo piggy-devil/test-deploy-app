74feb2acca42e522058a46de1e5ed714255e78e0
piggy-devil <muupaza@gmail.com>
2024-12-07 21:59:26 +0700

    ทดสอบ deploy

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant="outline">Test Deploy</Button>
+      <Button variant="outline">ทดสอบ Deploy</Button>
       <br />
       <Button variant="outline">Deploy Test</Button>
     </main>
