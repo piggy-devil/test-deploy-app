c9a62bfb97758aa7d59fce64c4c210c5feef7e4a
piggy-devil <muupaza@gmail.com>
2024-12-07 21:49:17 +0700

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant="outline">Test Deploy</Button>
+      <Button variant="outline">Test New Deploy</Button>
     </main>
   );
 }
