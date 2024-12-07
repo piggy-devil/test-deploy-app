5a2b8d6588dc04f9bfb60f409017974e88d0555c
piggy-devil <muupaza@gmail.com>
2024-12-07 21:56:57 +0700

    f

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant="outline">Test New Deploy</Button>
+      <Button variant="outline">Test Deploy</Button>
+      <br />
+      <Button variant="outline">Deploy Test</Button>
     </main>
   );
 }
